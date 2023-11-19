d4dda1431b130aa309836a7e49a7f3dec1486d7a
Artem <shav20@yandex.ru>
2023-11-20 01:21:01 +0400

    Тесты на переполнение площади круга

--- ShapeLib.Tests/CircleTests.cs
+++ ShapeLib.Tests/CircleTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using NUnit.Framework.Constraints;
+﻿using NUnit.Framework;
 
 namespace ShapeLib.Tests
 {
@@ -38,8 +32,9 @@ namespace ShapeLib.Tests
       (radius: 0.00001, expectedSquare: 0.000000000314159265358979323846),
       (radius: 1.0, expectedSquare: Math.PI),
       (radius: 7.77, expectedSquare: 189.6670591159),
-      (radius: 123456.789, expectedSquare: 47882831830.7088432141),
       (radius: 1000, expectedSquare: 3141592.65358979323846),
+      (radius: 123456.789, expectedSquare: 47882831830.7088432141),
+      (radius: 7.56e+153, expectedSquare: 1.7955332988620959E+308),
     };
 
     [Test]
@@ -48,6 +43,17 @@ namespace ShapeLib.Tests
     {
       var circle = new Circle(args.radius);
       Assert.That(circle.Square, Is.EqualTo(args.expectedSquare).Within(MathUtils.DoubleNumbersEqualityTolerance));
+    }
+
+    [Test]
+    [TestCase(double.MaxValue)]
+    [TestCase(7.57e+153)]
+    [TestCase(1.2345678e+200)]
+    public void GetCircleSquare_ShouldReturnOverflowedValue_WhenCircleRadiusIsLarge(double radius)
+    {
+      var ddd = Math.Sqrt(double.MaxValue) / Math.PI;
+      var circle = new Circle(radius);
+      Assert.AreEqual(double.PositiveInfinity, circle.Square);
     }
   }
 }
