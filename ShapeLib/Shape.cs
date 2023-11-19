f6af7df6c4e96cd5e7b44eb0a9afa9dbc5e89949
Artem <shav20@yandex.ru>
2023-11-19 20:42:21 +0400

    Базовый тип геометрической фигуры

---
+++ ShapeLib/Shape.cs
@@ -0,0 +1,13 @@
+namespace ShapeLib
+{
+  /// <summary>
+  /// Геометрическая фигура.
+  /// </summary>
+  public abstract class Shape
+  {
+    /// <summary>
+    /// Площадь фигуры.
+    /// </summary>
+    public abstract double Square { get; }
+  }
+}
